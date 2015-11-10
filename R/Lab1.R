153e9c96a3a988663d6e75efbabc3c835756e4e7
thozh912 <thomaszhangswe@gmail.com>
2015-11-10 09:47:16 +0100

    init lab1

--- R/Lab1.R
+++ R/Lab1.R
@@ -1,3 +1,5 @@
 www = "http://www.ida.liu.se/~732A37/T1-9.dat"
 data <- read.delim(www, header = FALSE, sep="\t")
-colnames(data) <- c("NAT","100m","200m","400m","800m","1500m","3000m","Mara")
+colnames(data) <- c("NAT","100m(s)","200m(s)","400m(s)",
+                    "800m(min)","1500m(min)","3000m(min)","Mara(min)")
+
